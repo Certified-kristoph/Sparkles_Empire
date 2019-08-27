f83eb88872bb572982f6182947b8a3b90ab747dd
Kristos Akalugwu <kristosakalugwu@gmail.com>
2019-08-27 12:56:24 +0100

    Just another update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,8 +9,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+  Widget image_carousel = new Container(
+    height: 200.0,
+    child: new Carousel(
+      boxFit: BoxFit.cover,
+      images: [
+        AssetImage('images/bk(1).jpg'),
+      ],
+      autoplay: false,
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(milliseconds: 1000),
+    ),
+  );
+    
     return Scaffold(
       appBar: new AppBar(
+        elevation: 0.5,
         title: Text('Sparkles Empire'),
         backgroundColor: Colors.red,
         actions: <Widget>[
@@ -18,26 +33,90 @@ class _HomePageState extends State<HomePage> {
               Icons.search,
               color: Colors.white,
             ),
-             onPressed: (){},
-            ),
+            onPressed: () {},
+          ),
           new IconButton(
             icon: Icon(
               Icons.shopping_cart,
               color: Colors.white,
             ),
-             onPressed: (){},
-            ),
+            onPressed: () {},
+          ),
         ],
       ),
-
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
             //header part of the drawer
             new UserAccountsDrawerHeader(
-              accountName: Text('Kristos Akalugwu'),//User acoount name
-              accountEmail: Text('Kristosakalugwu@gmail.com'),//User account Email
-            )
+              accountName: Text('Kristos Akalugwu'), //User acoount name
+              accountEmail:
+                  Text('Kristosakalugwu@gmail.com'), //User account Email
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+
+            //Body
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help, color: Colors.blue),
+              ),
+            ),
           ],
         ),
       ),
