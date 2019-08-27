6500adf4e3dadcaf697882d6b6031a6aef84f9e8
Kristos Akalugwu <kristosakalugwu@gmail.com>
2019-08-27 11:14:50 +0100

    Hello First commit

---
+++ lib/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('Sparkles Empire'),
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          new IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: (){},
+          )
+        ],
+      ),
+    );
+  }
+}
